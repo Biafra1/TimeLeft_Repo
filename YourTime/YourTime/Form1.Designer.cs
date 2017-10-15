a112c223210e2e3348c484f16766377e0731c39a
DuliszewskiMichał\mduli <mduliszewski@gmail.com>
2017-10-15 18:12:39 +0200

    From which shows your Time

---
+++ YourTime/YourTime/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace YourTime {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dayBox = new System.Windows.Forms.NumericUpDown();
+            this.monthBox = new System.Windows.Forms.NumericUpDown();
+            this.yearBox = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dayBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monthBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yearBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Matura MT Script Capitals", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(300, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Time";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(138, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your birth date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(261, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "day:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(302, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "month:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(347, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "year:";
+            // 
+            // dayBox
+            // 
+            this.dayBox.Location = new System.Drawing.Point(264, 130);
+            this.dayBox.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.dayBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.dayBox.Name = "dayBox";
+            this.dayBox.Size = new System.Drawing.Size(37, 20);
+            this.dayBox.TabIndex = 8;
+            this.dayBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // monthBox
+            // 
+            this.monthBox.Location = new System.Drawing.Point(307, 130);
+            this.monthBox.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.monthBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.monthBox.Name = "monthBox";
+            this.monthBox.Size = new System.Drawing.Size(37, 20);
+            this.monthBox.TabIndex = 9;
+            this.monthBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // yearBox
+            // 
+            this.yearBox.Location = new System.Drawing.Point(350, 130);
+            this.yearBox.Maximum = new decimal(new int[] {
+            2017,
+            0,
+            0,
+            0});
+            this.yearBox.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.yearBox.Name = "yearBox";
+            this.yearBox.Size = new System.Drawing.Size(75, 20);
+            this.yearBox.TabIndex = 10;
+            this.yearBox.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.yearBox.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(80, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "You have been alive for:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Old English Text MT", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(295, 220);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(67, 27);
+            this.timeLabel.TabIndex = 13;
+            this.timeLabel.Text = "label6";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(174, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "You have: ";
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Old English Text MT", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.Location = new System.Drawing.Point(295, 276);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(67, 27);
+            this.timeLeftLabel.TabIndex = 16;
+            this.timeLeftLabel.Text = "label8";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(168, 328);
+            this.progressBar1.Maximum = 10000000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(392, 52);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 17;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.progressLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.progressLabel.Location = new System.Drawing.Point(231, 398);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(70, 26);
+            this.progressLabel.TabIndex = 18;
+            this.progressLabel.Text = "label7";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Europe",
+            "Asia",
+            "Africa",
+            "North America",
+            "South America"});
+            this.comboBox1.Location = new System.Drawing.Point(469, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(706, 480);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(469, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 15);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Region:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 497);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.yearBox);
+            this.Controls.Add(this.monthBox);
+            this.Controls.Add(this.dayBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Your Time";
+            ((System.ComponentModel.ISupportInitialize)(this.dayBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monthBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yearBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown dayBox;
+        private System.Windows.Forms.NumericUpDown monthBox;
+        private System.Windows.Forms.NumericUpDown yearBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ YourTime/YourTime/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YourTime {
+    public partial class Form1 : Form {
+
+        double timeAliveInSeconds;
+        double lifeSpan = 2230000000; //seconds
+        double timeLeft;
+        double timeLeftInPercent;
+        bool isDead;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            isDead = false;
+            DateTime yourDate = new DateTime((int)yearBox.Value, (int)monthBox.Value, (int)dayBox.Value);
+            DateTime today = DateTime.Today;
+            TimeSpan timeAlive = today.Subtract(yourDate);
+            timeAliveInSeconds = timeAlive.TotalSeconds;
+
+            timeLabel.Text = "" + timeAliveInSeconds+" seconds.";
+            timeLeft = lifeSpan - timeAliveInSeconds;
+            if (timeLeft < 0) {
+                isDead = true;
+            }
+
+            timeLeftLabel.Text = "" + timeLeft+" seconds left to live..";
+            //timeLeftInPercent = (inSeconds / timeLeft) + "%";
+
+            if (!isDead) {
+                progressBar1.Value = (int)(timeAliveInSeconds / 223);
+            }
+
+
+            timer1.Start();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            timeAliveInSeconds++;
+            timeLabel.Text = "" + timeAliveInSeconds + " seconds.";
+            if (timeLeft < 0) {
+                timeLeftLabel.Text = "You should be dead by now!";
+            } else {
+                timeLeft--;
+                timeLeftLabel.Text = "" + timeLeft + " seconds left to live..";
+            }
+
+
+            timeLeftInPercent = (timeAliveInSeconds/lifeSpan)*100;
+            progressLabel.Text = timeLeftInPercent+"% finished.";
+            if (!isDead) {
+                progressBar1.Value = (int)(timeAliveInSeconds / 223);
+            }
+        }
+        
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e) {
+            switch (comboBox1.SelectedItem) {
+                case "Europe":
+                    lifeSpan *= 1.06;
+                    break;
+                case "Africa":
+                    lifeSpan *= 0.89;
+                    break;
+                case "Asia":
+                    lifeSpan = 2230000000;
+                    break;
+                case "North America":
+                    lifeSpan *= 1.07;
+                    break;
+                case "South America":
+                    lifeSpan *= 1.04;
+                    break;
+                default:
+                    lifeSpan = 2230000000;
+                    break;
+            }
+        }
+    }
+}
